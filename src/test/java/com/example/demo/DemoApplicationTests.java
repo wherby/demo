96af8c618c3640c6e8a7c53e858415afdd05b1e5
wherby <187225577@qq.com>
2024-09-25 09:52:15 +0800

    update test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,10 +2,12 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
 
 	@Autowired
@@ -15,8 +17,5 @@ class DemoApplicationTests {
 	}
 
 
-	void addIncidentShouldSuccess() throws Exception{
-
-	}
 
 }
